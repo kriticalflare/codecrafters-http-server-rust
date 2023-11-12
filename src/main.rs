832df50188fdfdb41e5d345bcb05959decbd62eb
kriticalflare <krithiksuvarna@gmail.com>
2023-11-12 12:44:00 +0530

    third stage

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 // Uncomment this block to pass the first stage
-use std::io::Write;
+use std::io::{BufRead, BufReader, Write};
 use std::net::TcpListener;
 
 fn main() {
@@ -14,9 +14,35 @@ fn main() {
         match stream {
             Ok(mut stream) => {
                 println!("accepted new connection");
-                let _ = stream
-                    .write("HTTP/1.1 200 OK\r\n\r\n".as_bytes())
-                    .map_err(|err| eprintln!("failed to write to stream {err}"));
+
+                let mut request_buffer = String::new();
+                let mut buf_reader = BufReader::new(&stream);
+
+                if let Ok(read_bytes) = buf_reader
+                    .read_line(&mut request_buffer)
+                    .map_err(|err| eprintln!("failed to read request {err}"))
+                {
+                    println!("read bytes : {read_bytes}");
+                    if read_bytes == 0 {
+                        continue;
+                    }
+
+                    let mut segments = request_buffer.split_whitespace();
+                    let _ = segments.next().unwrap();
+                    let path = segments.next().unwrap();
+
+                    println!("path -> {path}");
+
+                    if path == "/" {
+                        let _ = stream
+                            .write("HTTP/1.1 200 OK\r\n\r\n".as_bytes())
+                            .map_err(|err| eprintln!("failed to write to stream {err}"));
+                    } else {
+                        let _ = stream
+                            .write("HTTP/1.1 404 Not Found\r\n\r\n".as_bytes())
+                            .map_err(|err| eprintln!("failed to write to stream {err}"));
+                    }
+                }
                 let _ = stream
                     .shutdown(std::net::Shutdown::Both)
                     .map_err(|err| eprintln!("failed to shutdown connection {err}"));
